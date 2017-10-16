c8a5aed4d9edb6efba6e1f25720c9318236f6c15
endar <endarsasmito1@gmail.com>
2017-10-16 23:38:13 +0700

    upload db form untuk field pekerjaan dan alamat

--- rentalmobil.sql
+++ rentalmobil.sql
@@ -1,64 +1,76 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2017 at 07:46 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Generation Time: 16 Okt 2017 pada 18.36
+-- Versi Server: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `rentalmobil`
 --
-CREATE DATABASE IF NOT EXISTS `rentalmobil` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `rentalmobil`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kendaraan`
+-- Struktur dari tabel `user`
 --
 
-CREATE TABLE IF NOT EXISTS `kendaraan` (
-  `idKendaraan` int(11) NOT NULL AUTO_INCREMENT,
-  `namaKendaraan` text NOT NULL,
-  `merkKendaraan` varchar(255) NOT NULL,
-  `nopolKendaraan` varchar(255) NOT NULL,
-  `idPemilik` int(11) NOT NULL,
-  `statusKendaraan` varchar(50) NOT NULL,
-  PRIMARY KEY (`idKendaraan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
   `namaUser` text NOT NULL,
   `role` varchar(255) NOT NULL,
   `emailUser` varchar(255) NOT NULL,
   `passwordUser` varchar(50) NOT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `alamatUser` varchar(255) NOT NULL,
+  `pekerjaanUser` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`idUser`, `namaUser`, `role`, `emailUser`, `passwordUser`) VALUES
-(1, 'lalala', '', 'lalala', 'lalala');
+INSERT INTO `user` (`idUser`, `namaUser`, `role`, `emailUser`, `passwordUser`, `alamatUser`, `pekerjaanUser`) VALUES
+(1, 'lalala', '', 'lalala', 'lalala', '0', '0'),
+(2, 'Endar', '', 'endar@gmail.com', '12345', '0', '0'),
+(3, 'endar', '', 'endaasasr@gmail.com', '1234', '0', '0'),
+(4, '', '', '', '', '0', '0'),
+(5, '', '', '', '', '0', '0'),
+(6, '', '', '', '', '0', '0'),
+(7, 'endar', '', 'sas', 'asdas', '0', '0'),
+(8, 'qweqwe', '', 'qweqwe', 'qweqwe', 'qweqwe', 'qweqwe');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

aff9bb5d2301834a3687355297a267f559beed1f
endar <endarsasmito1@gmail.com>
2017-10-17 00:52:44 +0700

    penambahan homepage dan update controller

---
+++ application/views/homepage.php
@@ -0,0 +1,26 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="<?php echo base_url()."assets/vendor/bootstrap/css/bootstrap.min.css"; ?>" rel="stylesheet">
+        <link href="<?php echo base_url()."assets/vendor/bootstrap/css/bootstrap.css"; ?>" rel="stylesheet">
+        <link href="<?php echo base_url()."assets/vendor/css/rental.css"; ?>" rel="stylesheet">
+    </head>
+    <title>RENTAL | HOME</title>
+    <body class="home">
+        <nav></nav>
+        <br><br><jumbotron><h1 class="text-center">Selamat <strong>Datang</strong></h1></jumbotron><br><br>
+        <div class="row">
+            <div class="container">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <?php echo form_open_multipart('User/login/'); ?>
+                            
+                    <br><p class="text-center"><a href="<?php echo base_url()."index.php/User/regview";?>"><span>Buat sekarang</span></a></p>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </body>
+</html>
